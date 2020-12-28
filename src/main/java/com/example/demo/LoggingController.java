783cc17a2dee7fbdea7d9101b61621efa72da5bd
jesusmatosp <jesus.matosp@gmail.com>
2020-12-28 11:19:59 -0500

    fix

--- src/main/java/com/example/demo/LoggingController.java
+++ src/main/java/com/example/demo/LoggingController.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class LoggingController {
 
     Logger logger = LoggerFactory.getLogger(LoggingController.class);
+    private static final org.slf4j.Logger Logger =
+            org.slf4j.LoggerFactory.getLogger(LoggingController.class);
+    
+    Journal journal;
 
+    @Autowired
+   public LoggingController(Journal journal) {
+	 this.journal = journal;
+   }
+    
     @GetMapping("/get")
     public String index() {
     	
@@ -22,7 +32,9 @@ public class LoggingController {
         logger.info("An INFO Message response:" + json);
         logger.warn("A WARN Message");
         logger.error("An ERROR Message");
-        
+
+        journal.log("Message sent to journal DB");
+        Logger.error("Something else is wrong here");
         System.out.println("JESUS MATOS");
 
         return "Howdy! Check out the Logs to see the output...";
